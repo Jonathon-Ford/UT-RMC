b8fe2c5135fefe7faadea9831f4d60aa6baf4b64
Jonathon-Ford <61033062+Jonathon-Ford@users.noreply.github.com>
2020-02-27 20:03:48 -0500

    Update Imagination Station.py

--- Imagination Station.py
+++ Imagination Station.py
@@ -2,10 +2,8 @@
 # This program will ask for a set of instructions to run with the options "Forward, Backward, Turn Left, Turn Right
 
 
-import RPi.GPIO as GPIO #Pin setup for Entire Pi
+import RPi.GPIO as GPIO  # Pin setup for Entire Pi
 import time
-
-class Station:
 
 # pin setup
 GPIO.setmode(GPIO.BOARD)
@@ -14,28 +12,16 @@ GPIO.setup(22, GPIO.OUT)
 GPIO.setup(15, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 
-#motor variables
-FR = GPIO.PWM(13,50)#Front Right Motor #The value 50 is the Frequency
-FL = GPIO.PWM(22,50)#Front Left Motor #The value 12 is the GPIO pin
-RR = GPIO.PWM(15,50)#Rear Right Motor
-RL = GPIO.PWM(18,50)#Rear Left Motor
+# motor variables
+FR = GPIO.PWM(13, 50)  # Front Right Motor #The value 50 is the Frequency
+FL = GPIO.PWM(22, 50)  # Front Left Motor #The value 12 is the GPIO pin
+RR = GPIO.PWM(15, 50)  # Rear Right Motor
+RL = GPIO.PWM(18, 50)  # Rear Left Motor
 FR.start(100)
 FL.start(100)
 RR.start(100)
 RL.start(100)
 
-#Define functions for kids to use
-
-def forward(length):
-    FR.ChangeDutyCycle(6.5)
-    FL.ChangeDutyCycle(8)
-    RR.ChangeDutyCycle(6.5)
-    RL.ChangeDutyCycle(8)
-    time.sleep(length)
-    FR.ChangeDutyCycle(100)
-    FL.ChangeDutyCycle(100)
-    RR.ChangeDutyCycle(100)
-    RL.ChangeDutyCycle(100)
 
 def turn_right():
     FR.ChangeDutyCycle(10)
@@ -48,6 +34,7 @@ def turn_right():
     RR.ChangeDutyCycle(100)
     RL.ChangeDutyCycle(100)
 
+
 def turn_left():
     FR.ChangeDutyCycle(5)
     FL.ChangeDutyCycle(5)
@@ -59,21 +46,56 @@ def turn_left():
     RR.ChangeDutyCycle(100)
     RL.ChangeDutyCycle(100)
 
+
 def stop():
     FR.ChangeDutyCycle(100)
     FL.ChangeDutyCycle(100)
     RR.ChangeDutyCycle(100)
     RL.ChangeDutyCycle(100)
 
-def reverse():
-    FR.ChangeDutyCycle(8)
-    FL.ChangeDutyCycle(6.5)
-    RR.ChangeDutyCycle(8)
-    RL.ChangeDutyCycle(6.5)
 
-#Print instructions
-print("To go forward type forward(time): where time is how long the robot runs")
-print("To turn right 90 degrees type turn_right()")
-print("To turn left 90 degrees type turn_left()")
-print("To stop type stop(), and to abort press q")
-print("Using these functions program the robot through the obstacle course!")
+class Station:
+
+    # Define functions for kids to use
+
+    def forward(self):
+        FR.ChangeDutyCycle(6.5)
+        FL.ChangeDutyCycle(8)
+        RR.ChangeDutyCycle(6.5)
+        RL.ChangeDutyCycle(8)
+        time.sleep(self)
+        FR.ChangeDutyCycle(100)
+        FL.ChangeDutyCycle(100)
+        RR.ChangeDutyCycle(100)
+        RL.ChangeDutyCycle(100)
+
+    def reverse(self):
+        FR.ChangeDutyCycle(8)
+        FL.ChangeDutyCycle(6.5)
+        RR.ChangeDutyCycle(8)
+        RL.ChangeDutyCycle(6.5)
+        time.sleep(self)
+        FR.ChangeDutyCycle(100)
+        FL.ChangeDutyCycle(100)
+        RR.ChangeDutyCycle(100)
+        RL.ChangeDutyCycle(100)
+
+    # Print instructions
+    print("To go forward type forward(Time) and press enter. Where time is how long the robot runs")
+    print("To go backwards type reverse(Time) and press enter. Where time is how long the robot runs")
+    print("To turn right 90 degrees type turn_right() and press enter")
+    print("To turn left 90 degrees type turn_left() and press enter")
+    print("Using these functions program the robot through the obstacle course!")
+    print("Type all your instructions in at once. When you enter Start, your program will run. Good Luck!")
+
+    # Obtain inputs and push them to an array
+    command = 0
+    instructions = []  # Array that stores the kids code
+    while command != "Start":  # While the command is not Start continue taking commands and save them to the array
+        command = input()
+        instructions.append(command)
+
+    # Read the instructions given and run the functions
+    print("Your output is:")
+    for x in instructions:
+        eval(x)
